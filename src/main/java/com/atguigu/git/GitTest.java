ddd990225f42c14dbe41a194639c4c88d7ad05f9
Layne <Layne@atguigu.com>
2022-04-30 10:13:14 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author kabuda
+ * @create 2022-04-30 10:08
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
